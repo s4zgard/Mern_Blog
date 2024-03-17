ffdb8e8393379ca67586b0b14608cf8eac0de2de
s4zgard <s4zgard@gmail.com>
2024-03-17 13:25:41 +0500

    Initial Commit

---
+++ client/src/main.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const el = document.querySelector("#root");
+
+const root = ReactDOM.createRoot(el);
+
+root.render(<App />);
