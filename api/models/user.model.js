d9d65679c661a2cce84c94b8d8edf96bc8292148
s4zgard <s4zgard@gmail.com>
2024-03-17 18:04:02 +0500

    User Modle

---
+++ api/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      min: 3,
+      max: 20,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      min: 8,
+      max: 15,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
