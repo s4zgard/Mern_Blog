029d82f99a6b2a806b12c5e7d7e46143bf1097da
s4zgard <s4zgard@gmail.com>
2024-03-17 17:15:44 +0500

    Connected to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => console.log("MongoDB is Connected"));
 
 const app = express();
 
