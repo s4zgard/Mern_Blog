6b37b17cef1ac1f1ea124381d04557c9d4186aa1
s4zgard <s4zgard@gmail.com>
2024-03-19 14:59:30 +0500

    Sign In Route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, password, email } = req.body;
@@ -22,6 +23,40 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.json({ message: "Signup was successfull" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    errorHandler(400, "Please enter Email & Password");
+  }
+  try {
+    const loggedInUser = await User.findOne({ email });
+    if (!loggedInUser) {
+      return next(errorHandler(400, "Invalid Email/Password"));
+    }
+    const loggedInPassword = bcryptjs.compareSync(
+      password,
+      loggedInUser.password
+    );
+    if (!loggedInPassword) {
+      return next(errorHandler(400, "Invalid Email/Password"));
+    }
+
+    const { password: uPass, ...rest } = loggedInUser._doc;
+
+    const token = jwt.sign(
+      { id: loggedInUser._id, username: loggedInUser.username },
+      process.env.JWTSECRET,
+      { expiresIn: "1d" }
+    );
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
   } catch (error) {
     next(error);
   }
