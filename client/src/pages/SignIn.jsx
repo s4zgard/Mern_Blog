1fa5f430227d8c251d712d4854661f72c8cc950d
s4zgard <s4zgard@gmail.com>
2024-03-20 14:20:02 +0500

    Sign Functionality with Form

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,3 +1,18 @@
+import SignInForm from "../components/SignInForm";
+import Greeting from "../components/signup/Greeting";
+
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div className="mt-20 min-h-screen">
+      <div className="flex flex-col md:flex-row md:items-center max-w-3xl p-3 mx-auto md:space-x-4">
+        <div className="flex-1">
+          <Greeting />
+        </div>
+
+        <div className="flex-1">
+          <SignInForm />
+        </div>
+      </div>
+    </div>
+  );
 }
